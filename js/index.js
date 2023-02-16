af41c7c9d7dd2a56b25162499df86046b1c40f45
Thoma <thomaboehi@gmail.com>
2023-02-16 16:16:40 +0100

    Added copyright element

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Fonction pour ouvrir le burger menu
 function openBurgerMenu() {
   let x = document.getElementById("menuLinks");
   let b = document.getElementById("burgerMenuButton")
@@ -14,7 +15,7 @@ function openBurgerMenu() {
   }
 }
 
-
+// Fonction pour adapter le bouton de newletter en fonction de certaines règles
 function subButtonOver() {
   let regexMail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   let email = document.getElementById("subEmail").value;
@@ -39,19 +40,17 @@ function subButtonOver() {
 
 }
 
-function hexToDecimal(hex) {
-  return parseInt(hex.replace("#",""), 16)
-}
-
+// Fonction pour remettre le texte Subscribe
 function subButtonOut() {
   let text = "Subscribe";
   document.getElementById("subButton").setAttribute("value", text)
 }
 
+
+// 2 Fonctions, une pour ouvrir la div about-longer une autre pour fermer
 function openAboutLonger() {
   document.getElementById("about-long").setAttribute("data-state", "open")
 }
-
 function closeAboutLonger() {
   document.getElementById("about-long").setAttribute("data-state", "close")
 }
